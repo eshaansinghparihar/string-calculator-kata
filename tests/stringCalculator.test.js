c40f4bc31f4be41e7c5ed0bfa745e296b5324af7
eshaansinghparihar <eshaan.singh.parihar@gmail.com>
2024-10-19 13:14:27 +0530

    Test for empty string

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,6 +1,14 @@
+const stringCalculator = require('../module/stringCalculator');
+
 test(('test for nothing'),()=>{
     function nothing(){
         return;
     }
     expect(nothing()).toBe();
-})
+});
+
+test(('empty string should return 0'),()=>{
+    const sum = stringCalculator.add("")
+    expect(sum).toBe(0);
+});
+
