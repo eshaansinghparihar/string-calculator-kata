bdd1c2bd8e8fd5065a3ec7d096602d795247bd5a
eshaansinghparihar <eshaan.singh.parihar@gmail.com>
2024-10-19 13:45:04 +0530

    Test should work for unknown number of numbers

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,23 +1,39 @@
 const stringCalculator = require('../module/stringCalculator');
 
-test(('test for nothing'),()=>{
-    function nothing(){
-        return;
-    }
-    expect(nothing()).toBe();
+describe('First Test',()=>{
+
+    it(('test for nothing'),()=>{
+        function nothing(){
+            return;
+        }
+        expect(nothing()).toBe();
+    });
 });
 
-test(('empty string should return 0'),()=>{
-    const sum = stringCalculator.add("");
-    expect(sum).toBe(0);
+describe('Test for known amount of numbers', () => {
+
+    it(('empty string should return 0'),()=>{
+        const sum = stringCalculator.add("");
+        expect(sum).toBe(0);
+    });
+    
+    it(('should return 1 for "1"'),()=>{
+        const sum = stringCalculator.add("1");
+        expect(sum).toBe(1);
+    });
+    
+    it('should return 6 for "1,5',()=>{
+        const sum = stringCalculator.add("1,5");
+        expect(sum).toBe(6);
+    });
+
 });
 
-test(('should return 1 for "1"'),()=>{
-    const sum = stringCalculator.add("1");
-    expect(sum).toBe(1);
-});
+describe('Test for unknown amount of numbers', ()=>{
 
-test('should return 6 for "1,5',()=>{
-    const sum = stringCalculator.add("1,5");
-    expect(sum).toBe(6);
-});
+    it('should work for unknown anount of numbers',()=>{
+        const sum = stringCalculator.add("1,5,1,5,2,2,3,3,7,7,11,11,13,13");
+        expect(sum).toBe(1+5+1+5+2+2+3+3+7+7+11+11+13+13);
+    });
+
+})
