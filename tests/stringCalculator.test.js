709e70a905f82b979363654ad26a0a35771c5be9
eshaansinghparihar <eshaan.singh.parihar@gmail.com>
2024-10-19 13:52:23 +0530

    Test to handle new lines between numbers

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -36,4 +36,13 @@ describe('Test for unknown amount of numbers', ()=>{
         expect(sum).toBe(1+5+1+5+2+2+3+3+7+7+11+11+13+13);
     });
 
+});
+
+describe('Test to handle new lines between numbers',()=>{
+
+    it('should return 6 for 1\n2,3',()=>{
+        const sum = stringCalculator.add('1\n2,3');
+        expect(sum).toBe(6);
+    })
+
 })
