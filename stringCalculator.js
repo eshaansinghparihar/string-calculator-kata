9f80977d411a62a99c38f9c874096e0120d98022
eshaansinghparihar <eshaan.singh.parihar@gmail.com>
2025-03-10 09:28:25 +0000

    deploy: c42dba0c12a52e59be37b9929f77755607cfb878

---
+++ stringCalculator.js
@@ -0,0 +1,28 @@
+function add(numbers){
+    if(typeof(numbers) === 'string')
+    {
+        if(numbers === '') return 0;
+    
+        let nums = numbers, delimeter = '', negatives = [];
+        if(numbers.startsWith('//')){
+            delimeter = nums.substring(2,nums.indexOf('\n'));
+            if(delimeter.startsWith('[') && delimeter.endsWith(']'))
+                delimeter = delimeter.replace(/[\[\]]/g, '');
+            nums = numbers.slice(numbers.indexOf('\n'));
+        }
+        nums = (delimeter) ? nums.split(delimeter).map(Number) : nums.split(/[\n,]/).map(Number);
+        
+        nums.forEach(number=>{
+            if(number < 0)
+                negatives.push(number)
+        });
+    
+        if (negatives.length) {
+            throw new Error(`negative numbers not allowed: ${negatives.join(', ')}`);
+        }
+    
+        return nums.filter(num => num <= 1000).reduce((sum, num) => sum + num, 0);
+    }
+    }
+
+module.exports = {add};
